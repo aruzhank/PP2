5c22ff122d4e6adc0babb0819916c564ef9ca003
unknown <aruzhan0134@gmail.comgit config --global user.name aruzhankgit config --user.email aruzhan0134@gmail.com>
2019-02-15 05:05:18 +0600

    ready

---
+++ Week2/task3/task3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        public static void PrintSpaces(int n)                                    //creating a function
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write("   ");
+        }
+
+        public static void Dir(DirectoryInfo dir, int a)                         //creating a function that will show every folder and file 
+        {
+            FileInfo[] files = dir.GetFiles();                                   //getting files
+            DirectoryInfo[] directories = dir.GetDirectories();                  //getting directories
+
+            foreach (FileInfo file in files)                                     //considering each file
+            {
+                PrintSpaces(a);
+                Console.WriteLine(file.Name);                                    //getting file's name
+            }
+
+            foreach (DirectoryInfo direct in directories)                        //considering each directory
+            {
+                PrintSpaces(a);
+                Console.WriteLine(direct.Name);                                  //getting directory's name
+                Dir(dir, a + 1);                                                 //recursion
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"C:\Users\Acer\Downloads");      //getting access to directory
+            Dir(d, 0);                                                            //calling function
+            Console.ReadKey();
+        }
+    }
+}
