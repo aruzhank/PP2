afffeb53f35a9763ddf5a3f24eb10d492dd3950d
unknown <aruzhan0134@gmail.comgit config --global user.name aruzhankgit config --user.email aruzhan0134@gmail.com>
2019-02-14 22:49:27 +0600

    ready

---
+++ Week2/task4/task4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir1 = "folder1";                                         
+            string dir2 = "folder2";                                        
+            string text = "example.txt";
+
+            string s = (@"C:\Users\Acer");                                    //directory's path, where folders and files will be created
+            string p1 = Path.Combine(s, dir1);                               //path to the "folder1"
+            string p2 = Path.Combine(s, dir2);                               //path to the "folder2"
+          
+            Directory.CreateDirectory(p1);                                    //create "folder1"
+
+            string fileSource = Path.Combine(p1, text);                       //path to the given file
+
+            FileStream fs = File.Create(fileSource);                          //stream to create file by given path
+            fs.Close();                                                       //close file
+            
+            Directory.CreateDirectory(p2);                                    //create "folder2"
+
+            string fileDest = Path.Combine(p2, text);                         //path to the folder, where we copy file
+            File.Copy(fileSource, fileDest);                                  //copy a file to "folder2"
+            File.Delete(fileSource);                                          //delete original file
+            Directory.Delete(p1);                                             //delete folder, where file was located
+        }
+    }
+}
+
