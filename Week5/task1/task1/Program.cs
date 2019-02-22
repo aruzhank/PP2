1b5e9bcc4d31c9e5c20411be2855562911da916c
unknown <aruzhan0134@gmail.comgit config --global user.name aruzhankgit config --user.email aruzhan0134@gmail.com>
2019-02-22 13:11:32 +0600

    ready

---
+++ Week5/task1/task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace task1
+{
+    public class Complex
+    {
+        public double a;
+        public double b;
+        public Complex()
+        {
+            a = 8;
+            b = 7;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F2();
+        }
+
+        private static void F2()
+        {
+            FileStream fs = new FileStream("complex.txt", FileMode.Open, FileAccess.Read);
+            XmlSerializer xml = new XmlSerializer(typeof(Complex));
+            Complex cm = xml.Deserialize(fs) as Complex;
+            fs.Close();
+        }
+
+        private static void F1()
+        {
+            Complex c = new Complex();
+            FileStream fs = new FileStream("complex.txt", FileMode.Create, FileAccess.Write);
+            XmlSerializer xml = new XmlSerializer(typeof(Complex));
+            xml.Serialize(fs, c);
+            fs.Close();
+        }
+    }
+}
